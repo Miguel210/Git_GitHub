5f3c245b67e3878ba433c4f987ac8bcf9eab4466
Miguel Angel <mickyjunior04@gmail.com>
2022-06-21 20:44:47 -0500

    Creacion de interfaz principal

---
+++ src/view/frmMenu.java
@@ -0,0 +1,15 @@
+
+package view;
+
+import javax.swing.JFrame;
+
+public class frmMenu extends JFrame {
+
+    public frmMenu() {
+        this.setTitle("Hola");
+        this.setSize(100, 100);
+        this.setVisible(true);
+    }
+    
+    
+}
